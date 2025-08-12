ea5a296793db99af39b6296d14eba8a589f622d2
mourahr <mourahr@gmail.com>
2025-08-11 22:22:55 -0300

    including escolar code scratch

--- README.md
+++ README.md
@@ -3,15 +3,19 @@
 <p>Here you will see how a beginner Java learner create confused codes (this may provide some funny moments 🙂).</p>
 <p>Let's try to apply best (and some others) practices of this language</p>
 
-<h2> FIRST PROJECT - CALCULATOR</h2>
+<h2> PROJECT - CALCULATOR</h2>
 <p>Here we are facing to a simple task.
 <li>Created a Main (default) java class.
 <li>Declared and initialized variables of Integer and Float type.
 <li>Created methods for calculate the four main signs of Math.
 <li>Used different ways to format the output and show on display.
 
-<h2> SECOND PROJECT - PARTY ENTRY CHECK</h2>
+<h2> PROJECT - MAIORIDADE</h2>
 <p>First steps on conditionals.
 <li>Added the Scanner class to read and load on a variable the user input
 <li>Declared and initialized variables of Boolean type.
 <li>Used <i>if</i> statement to control the flow related a specific conditional based on age of user.
+
+<h2> PROJECT - ESCOLAR</h2>
+<p>Using conditional switch/case.
+<li>Breaking code in smallest blocks
--- escolar/src/Main.java
+++ escolar/src/Main.java
@@ -1,18 +1,48 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         bannerAplicacao();
+        String materia = escolheMateria();
+        System.out.printf("Materia escolhida: %s\n", materia);
+        coletaProvasNotas(materia);
+    }
+
+    private static void coletaProvasNotas(String materia) {
+        int avaliacoes = carregaQtdadeAvaliacoes();
+        var lista = carregaNotas(avaliacoes);
+        System.out.println(Arrays.toString(lista));
+    }
+
+    private static float[] carregaNotas(int avaliacoes) {
+        Scanner scanner = new Scanner(System.in);
+        float[] notas = new float[avaliacoes];
+        for (int cont = 0; cont < avaliacoes; cont++) {
+            System.out.printf("\nInforme a nota %d: ", cont+1);
+            float nota = scanner.nextFloat();
+            notas[cont] = nota;
+        }
+        return notas;
+    }
+
+    private static int carregaQtdadeAvaliacoes() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Informe a quantidade de avaliacoes: ");
+        return scanner.nextInt();
+}
+
+    private static String escolheMateria() {
         switch (iniciaJornadaProfessor()) {
             case 1:
-                System.out.println("Matéria Português!");
-                break;
+                return "Portugues";
             case 2:
-                System.out.println("Matéria Matemátca");
-                break;
+                return "Matematica";
             default:
                 System.out.println("Opção inválida. Tente novamente");
+                escolheMateria();
         }
+        return "null";
     }
 
     private static int iniciaJornadaProfessor() {
